11fa247f592d3e0f78387a47c642bb1e74d8c036
brittleru <diemetal851@gmail.com>
2025-08-08 17:37:03 +0300

    Remover unnecessary files. Updated some information (need to change more here for the contents). Optimized head for SEO and improved some performance by deferring, lazy loading and changing the format of photos to a more compressed format.

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# My Personal Site 
+# Sebastian Mocanu - AI Research Scientist Portfolio
 
-### This is where I put my projects and how you can contact me with other methods
+Personal portfolio website showcasing my research in AI, Computer Vision, and Robotics and other development
+and engineering projects.
+
+**Live Site:** [sebastianmocanu.com](https://sebastianmocanu.com)
+
+### About Me
+
+Welcome to my personal portfolio! I'm Sebastian Mocanu, an AI Research Scientist and PhD candidate at University
+POLITEHNICA of Bucharest, specializing in Computer Vision and Robotics. This website showcases my projects,
+research work, and professional journey.
+
+### Tech Stack
+
+- **Frontend:** HTML5, CSS3, JavaScript (ES+6)
+- **Styling:** Bootstrap 4, Custom CSS with CSS Grid & Flexbox
+- **Animations:** [AOS](https://github.com/michalsnik/aos) (Animate On Scroll),
+  [Particle.js](https://github.com/VincentGarreau/particles.js), Custom CSS animations
+- **Icons:** Font Awesome 5
+- **Performance:** WebP images, lazy loading, CDN resources
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+### Contact
+
+Sebastian Mocanu - AI Research Scientist & PhD Candidate
+
+- 🌐 Website: [sebastianmocanu.com](https://sebastianmocanu.com)
+- 💼 LinkedIn: [Sebastian Mocanu](https://www.linkedin.com/in/sebastian-mocanu-b76a61184/)
+- 🐙 GitHub: [@brittleru](https://github.com/brittleru)
+- 📚 Google Scholar: [Sebastian Mocanu](https://scholar.google.com/citations?user=osyBED4AAAAJ&hl=en&oi=ao)
+- 🔬 ORCID: [0009-0007-0313-4724](https://orcid.org/0009-0007-0313-4724)
--- scripts/textWriting.js
+++ scripts/textWriting.js
@@ -1,55 +1,48 @@
 const titleID = document.getElementById("writing-text");
 const passionID = document.getElementById("writing-text-passion");
-const blinkTitle = document.getElementById("blinking-effect");
 
 let i = 0;
 let j = 0;
-const titleText = "Web Developer";
-const passionText = "< Deep Learning and Guitar enthusiast />";
+const titleText = "Research Scientist";
+const passionText = "< AI, Computer Vision & Robotics />";
 const textWriteSpeed = 100;
 
 
 function blinkStop() {
-  setTimeout(function() {
-    titleID.innerHTML = "";
-    typingText();
-    setTimeout(function() {
-      passionID.innerHTML = "";
-      anotherText();
+    setTimeout(function () {
+        titleID.innerHTML = "";
+        typingText();
+        setTimeout(function () {
+            passionID.innerHTML = "";
+            anotherText();
+        }, 2000);
     }, 2000);
-  }, 2000);
 
 }
 
 function typingText() {
-  if (i < titleText.length) {
-    titleID.innerHTML += titleText[i];
-    i++;
-    setTimeout(typingText, textWriteSpeed);
+    if (i < titleText.length) {
+        titleID.innerHTML += titleText[i];
+        i++;
+        setTimeout(typingText, textWriteSpeed);
 
-    if (i === titleText.length) {
-      // titleID.innerHTML += `<span id="blinking-effect" class="blinking-dash writing-text-dash">_</span>`;
+        if (i === titleText.length) {
+            // titleID.innerHTML += `<span id="blinking-effect" class="blinking-dash writing-text-dash">_</span>`;
+        }
     }
-  }
 }
 
 function anotherText() {
-  if (j < passionText.length) {
-    passionID.innerHTML += passionText[j];
-    j++
+    if (j < passionText.length) {
+        passionID.innerHTML += passionText[j];
+        j++
 
-    setTimeout(anotherText, textWriteSpeed);
+        setTimeout(anotherText, textWriteSpeed);
 
-    if (j === passionText.length) {
-      passionID.innerHTML += `<span id="blinking-effect-2" class="blinking-dash writing-text-dash">_</span>`;
-      // titleID.innerHTML += `<span id="blinking-effect" class="blinking-dash writing-text-dash">_</span>`;
-      // TODO: Ask for pareri
-
-
+        if (j === passionText.length) {
+            passionID.innerHTML += `<span id="blinking-effect-2" class="blinking-dash writing-text-dash">_</span>`;
+        }
     }
-  }
 }
 
-
-// Event listeners
 window.addEventListener("load", blinkStop);
